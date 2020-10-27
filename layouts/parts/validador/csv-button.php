b7e8d598cdbb06a6c05affcce792fc0261f9ec59
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-10-27 16:10:18 -0300

    adiciona o slug como prefixo do editBox para evitar conflitos

--- layouts/parts/validador/csv-button.php
+++ layouts/parts/validador/csv-button.php
@@ -10,10 +10,10 @@ if ($inciso == 1){
     $route = MapasCulturais\App::i()->createUrl($slug, 'export_inciso1');    
     ?>
     
-    <a class="btn btn-default download btn-export-cancel"  ng-click="editbox.open('export-inciso1', $event)" rel="noopener noreferrer">CSV <?= $name ?></a>
+    <a class="btn btn-default download btn-export-cancel"  ng-click="editbox.open('<?= $slug ?>-export-inciso1', $event)" rel="noopener noreferrer">CSV <?= $name ?></a>
 
     <!-- Formulário -->
-    <edit-box id="export-inciso1" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso I') ?>" cancel-label="Cancelar" close-on-cancel="true">
+    <edit-box id="<?= $slug ?>-export-inciso1" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso I') ?>" cancel-label="Cancelar" close-on-cancel="true">
         <form class="form-export-<?= $slug ?>" action="<?=$route?>" method="POST">
       
             <label for="from">Data inícial</label>
@@ -32,16 +32,16 @@ if ($inciso == 1){
 else if ($inciso ==2){
     $route = MapasCulturais\App::i()->createUrl($slug, 'export_inciso2');   
     ?>
-    <a class="btn btn-default download form-export-clear" ng-click="editbox.open('export-inciso2', $event)" rel="noopener noreferrer">CSV <?= $name ?></a>
+    <a class="btn btn-default download form-export-clear" ng-click="editbox.open('<?= $slug ?>-export-inciso2', $event)" rel="noopener noreferrer">CSV <?= $name ?></a>
     
     <!-- Formulario para cpf -->
-    <edit-box id="export-inciso2" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso II') ?>" cancel-label="Cancelar" close-on-cancel="true">
+    <edit-box id="<?= $slug ?>-export-inciso2" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso II') ?>" cancel-label="Cancelar" close-on-cancel="true">
         <form class="form-export-<?= $slug ?>" action="<?=$route?>" method="POST">
       
             <label for="from">Data inícial</label>
             <input type="date" name="from" id="from">
             
-            <label for="from">Data final</label>  
+            <label for="to">Data final</label>  
             <input type="date" name="to" id="to">            
 
             <label for="type">Tipo de exportação (CPF ou CNPJ)</label>
